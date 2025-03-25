fe3a52b40b822ae3422fb106b5fd8b9c7214ced0
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-25 18:09:55 +0300

    (added) : ef core context

---
+++ TrackerDesktop/Data/TrackerContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrackerDesktop.Data.Entities;
+
+namespace TrackerDesktop.Data
+{
+    public class TrackerContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Mode> Modes { get; set; }
+        public DbSet<Step> Steps { get; set; }
+
+        public TrackerContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=TrackerDesktop.db");
+        }
+    }
+}
