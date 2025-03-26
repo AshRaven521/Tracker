296349ecbb4570ed4dde8674d2eaf4ae7f8b72e6
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-26 21:04:32 +0300

    (added) : property initialization

--- TrackerDesktop/Data/Entities/Mode.cs
+++ TrackerDesktop/Data/Entities/Mode.cs
@@ -3,7 +3,7 @@
     public class Mode
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int MaxBottleNumber { get; set; }
         public int MaxUsedTips { get; set; }
     }
