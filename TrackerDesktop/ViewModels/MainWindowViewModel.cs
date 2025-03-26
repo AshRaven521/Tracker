552465523d763ce35e4e707f70b0467f497f8fbf
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-26 21:07:12 +0300

    (added) : navigation between pages

--- TrackerDesktop/ViewModels/MainWindowViewModel.cs
+++ TrackerDesktop/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,45 @@
-﻿namespace TrackerDesktop.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TrackerDesktop.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+
+        [ObservableProperty]
+        private ViewModelBase? currentPage;
+
+        private readonly LogInPageViewModel logInPage;
+        private readonly RegistrationPageViewModel registrationPage;
+        private readonly HomePageViewModel homePage;
+
+        public MainWindowViewModel(LogInPageViewModel logInPage,
+                                   RegistrationPageViewModel registrationPage,
+                                   HomePageViewModel homePage)
+        {
+            this.logInPage = logInPage;
+            this.registrationPage = registrationPage;
+            this.homePage = homePage;
+            CurrentPage = null;
+        }
+
+        [RelayCommand]
+        public void GoToLogInPage()
+        {
+            CurrentPage = logInPage;
+        }
+
+        [RelayCommand]
+        public void GoRegistrationPage()
+        {
+            CurrentPage = registrationPage;
+        }
+
+        [RelayCommand]
+        public void GoToHomePage()
+        {
+            CurrentPage = homePage;
+        }
+
     }
 }
