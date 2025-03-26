4cfcc36ee71fd3a2768288370d88f78acfabafb7
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-26 21:05:43 +0300

    (added) : registration page

---
+++ TrackerDesktop/Views/RegistrationPageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TrackerDesktop.Views;
+
+public partial class RegistrationPageView : UserControl
+{
+    public RegistrationPageView()
+    {
+        InitializeComponent();
+    }
+}
