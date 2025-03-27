dbd1c06b39315dec7c803734c041d40f6e840733
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-27 22:16:46 +0300

    (added) : home page

---
+++ TrackerDesktop/Views/HomePageView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace TrackerDesktop.Views;
+
+public partial class HomePageView : UserControl
+{
+    public HomePageView()
+    {
+        InitializeComponent();
+    }
+}
