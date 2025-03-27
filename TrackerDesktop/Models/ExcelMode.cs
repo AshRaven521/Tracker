3ac14bd1514c4a466d869ef3124e17e0cea830ab
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-27 22:18:09 +0300

    (added) : model for excel export

---
+++ TrackerDesktop/Models/ExcelMode.cs
@@ -0,0 +1,10 @@
+﻿namespace TrackerDesktop.Models
+{
+    public class ExcelMode
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int MaxBottleNumber { get; set; }
+        public int MaxUsedTips { get; set; }
+    }
+}
---
+++ TrackerDesktop/Models/ExcelStep.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackerDesktop.Models
+{
+    public class ExcelStep
+    {
+        public int Id { get; set; }
+        public int ModeId { get; set; }
+        public int Timer { get; set; }
+        public string Destination { get; set; } = string.Empty;
+        public int Speed { get; set; }
+        public string Type { get; set; } = string.Empty;
+        public int Volume { get; set; }
+    }
+}
