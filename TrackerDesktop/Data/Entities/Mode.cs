75333825e0626cf98c0b09b4fa44a131502576ff
Ivan Kondrashov <ivan.kondrashov.92@mail.ru>
2025-03-27 22:22:56 +0300

    (added) : override equals, gethashcode fro comparing objects

--- TrackerDesktop/Data/Entities/Mode.cs
+++ TrackerDesktop/Data/Entities/Mode.cs
@@ -1,4 +1,6 @@
-﻿namespace TrackerDesktop.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace TrackerDesktop.Data.Entities
 {
     public class Mode
     {
@@ -6,5 +8,28 @@
         public string Name { get; set; } = string.Empty;
         public int MaxBottleNumber { get; set; }
         public int MaxUsedTips { get; set; }
+        public List<User?> Users { get; set; } = new List<User?>();
+
+        public override bool Equals(object? obj)
+        {
+            var item = obj as Mode;
+            if (item == null)
+            {
+                return false;
+            }
+
+            return (this.Id == item.Id)
+                && (this.Name == item.Name)
+                && (this.MaxBottleNumber == item.MaxBottleNumber)
+                && (this.MaxUsedTips == item.MaxUsedTips);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^
+                Name.GetHashCode() ^
+                MaxBottleNumber.GetHashCode() ^
+                MaxUsedTips.GetHashCode();
+        }
     }
 }
--- TrackerDesktop/Data/Entities/Step.cs
+++ TrackerDesktop/Data/Entities/Step.cs
@@ -1,15 +1,46 @@
-﻿namespace TrackerDesktop.Data.Entities
+﻿using System.Collections.Generic;
+
+namespace TrackerDesktop.Data.Entities
 {
     public class Step
     {
         public int Id { get; set; }
         public int ModeId { get; set; }
         public int Timer { get; set; }
-        public string? Destination { get; set; }
+        public string Destination { get; set; } = string.Empty;
         public int Speed { get; set; }
-        public string Type { get; set; }
+        public string Type { get; set; } = string.Empty;
         public int Volume { get; set; }
         public Mode Mode { get; set; } = new Mode();
+        public User? User { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            var item = obj as Step;
+            if (item == null)
+            {
+                return false;
+            }
+
+            return (this.Id == item.Id)
+                && (this.ModeId == item.ModeId)
+                && (this.Timer == item.Timer)
+                && (this.Destination == item.Destination)
+                && (this.Speed == item.Speed)
+                && (this.Type == item.Type)
+                && (this.Volume == item.Volume);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() ^
+                ModeId.GetHashCode() ^
+                Timer.GetHashCode() ^
+                Destination.GetHashCode() ^
+                Speed.GetHashCode() ^
+                Type.GetHashCode() ^
+                Volume.GetHashCode();
+        }
 
     }
 }
